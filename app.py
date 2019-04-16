da6aaac1f3f81cdfb4d41043c2c4c5f7de165ac6
Matt Bush <bush.matthewjames@gmail.com>
2019-04-16 17:59:27 +0100

    Create login route for login page

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template, url_for
-from forms import registerForm
+from forms import registerForm, loginForm
 from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
@@ -10,16 +10,20 @@ def create_app():
     app = Flask(__name__)
     csrf.init_app(app)
 
-# @app.route("/")
+@app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html')
 
-@app.route("/")
 @app.route("/register")
 def register():
     form = registerForm()
     return render_template('register.html', form=form)
+
+@app.route("/login")
+def login():
+    form = loginForm()
+    return render_template('login.html', form=form)
 
 
 if __name__ == "__main__":
