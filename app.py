434760cfa430c2aa9ef91919739d11c22904291c
Matt Bush <bush.matthewjames@gmail.com>
2019-04-16 17:38:26 +0100

    Add  flask-wtf forms to register template
    Add SECRET_KEY for csrf protection provided with flask-wtf

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
+import os
 from flask import Flask, render_template, url_for
+from forms import registerForm
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(32)
 
-@app.route("/")
+def create_app():
+    app = Flask(__name__)
+    csrf.init_app(app)
+
+# @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html')
 
+@app.route("/")
 @app.route("/register")
 def register():
-    return render_template('register.html')
+    form = registerForm()
+    return render_template('register.html', form=form)
 
 
 if __name__ == "__main__":
--- forms.py
+++ forms.py
@@ -3,6 +3,6 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length, Email
 
 class registerForm(FlaskForm):
-    companyName = StringField('companyName', validators=[DataRequired(), Length(min=3, max=20)])
-    companyEmail = StringField('companyEmail', validators=[DataRequired(), Email()])
+    companyName = StringField('Company Name', validators=[DataRequired(), Length(min=3, max=20)])
+    companyEmail = StringField('Company Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Register')
