7fac32b4b4569abba53ef1c264b7e36c74c6fba0
Matt Bush <bush.matthewjames@gmail.com>
2019-04-16 16:20:13 +0100

    Create register route and register template
    For register form. Test in browser

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
