8fb04edc9a76e6597cc24472de9b4fb732885583
Matt Bush <bush.matthewjames@gmail.com>
2019-04-17 13:46:24 +0100

    Add wtforms validators to register form
    Appropriate error messages for form validation

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 from forms import registerForm, loginForm
 from flask_wtf.csrf import CSRFProtect
 
@@ -10,15 +10,19 @@ def create_app():
     app = Flask(__name__)
     csrf.init_app(app)
 
-@app.route("/")
-@app.route("/home")
+@app.route('/')
+@app.route('/home')
 def home():
     return render_template('home.html')
 
-@app.route("/register")
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = registerForm()
+    if form.validate_on_submit():
+        return redirect(url_for('login'))
+    
     return render_template('register.html', form=form)
+    
 
 @app.route("/login")
 def login():
