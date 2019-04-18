3ea7f88baf60617cd3784b5c37e9215210b48b6e
Matt Bush <bush.matthewjames@gmail.com>
2019-04-18 16:50:50 +0100

    Update app.py comments for code readability

--- app.py
+++ app.py
@@ -5,25 +5,28 @@ from flask_sqlalchemy import SQLAlchemy
 from forms import registerForm, loginForm
 from flask_wtf.csrf import CSRFProtect
 
+# instance of flask 
 app = Flask(__name__)
+# evironment variables
 app.config['SECRET_KEY'] = os.urandom(32)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# instance of sqlalchemy 
 db = SQLAlchemy(app)
-
+# attach flask-wtf csrf token to app
 csrf.init_app(app)
 
-
+# create employer user db model 
 class employer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     companyName = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(50), nullable=False, default='default.jpg')
     job_posts = db.relationship('job_post', backref='author', lazy=True)
-
+    # string to return in shell when employer model called
     def __repr__(self):
         return f"User: {self.companyName}"
-
+# create job_post db model
 class job_post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
@@ -35,7 +38,7 @@ class job_post(db.Model):
     resp = db.Column(db.Text, nullable=False)
     requirements = db.Column(db.Text, nullable=False)
     emp_id = db.Column(db.Integer, db.ForeignKey('employer.id'), nullable=False )
-
+    # string to return in shell when job_post model called
     def __repr__(self):
         return f"Job Post: {self.date_posted}, {self.title}"
 
