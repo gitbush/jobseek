d211f338cc8ce4a0e33257a718bd2e29e3563182
Matt Bush <bush.matthewjames@gmail.com>
2019-04-25 17:20:27 +0100

    Add custom validation for user login
    Checks if user is already registered and raises error msg

--- jobseek/forms.py
+++ jobseek/forms.py
@@ -1,12 +1,26 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, Length, Email, ValidationError
+from jobseek.models import employer
 
+# wtforms registration class
 class registerForm(FlaskForm):
     companyName = StringField('Company Name', validators=[DataRequired(), Length(min=3, max=20)])
     companyEmail = StringField('Company Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Register')
 
+    # custom validation checking if a user has already signed up with company name and email
+    def validate_companyName(self, companyName):
+        emp = employer.query.filter_by(companyName=companyName.data).first()
+        if emp:
+            raise ValidationError('Company already exists on Jobseek. Please login')
+
+    def validate_Email(self, email):
+        emp = employer.query.filter_by(email=companyEmail.data).first()
+        if emp:
+            raise ValidationError('Email already exists on Jobseek. Please use an alternate email')
+
+# wtforms login class
 class loginForm(FlaskForm):
     companyName = StringField('Company Name', validators=[DataRequired(), Length(min=3, max=20)])
     companyEmail = StringField('Company Email', validators=[DataRequired(), Email()])
