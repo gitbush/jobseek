f170e639c227716afa58aa8b16fc5c40d81d12a9
Matt Bush <bush.matthewjames@gmail.com>
2019-04-25 18:14:34 +0100

    Add employer logout functionality
    Ends the session and returns to home page

--- jobseek/routes.py
+++ jobseek/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, redirect, flash
 from jobseek import app, db
 from jobseek.forms import registerForm, loginForm
 from jobseek.models import employer, job_post
-from flask_login import login_user, current_user
+from flask_login import login_user, current_user, logout_user
 
 @app.route('/')
 @app.route('/home')
@@ -27,7 +27,7 @@ def register():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect('home')
+        return redirect(url_for('home'))
     form = loginForm()
     if form.validate_on_submit():
         emp = employer.query.filter_by(email=form.companyEmail.data).first()
@@ -37,3 +37,8 @@ def login():
         else:
             flash(f'Login unsuccessful. Please check company name and email', 'danger')
     return render_template('login.html', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
