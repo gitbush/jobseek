deabf071006aa5082025c29e54df58b1b467c0ac
Matt Bush <bush.matthewjames@gmail.com>
2019-05-02 15:05:44 +0100

    Re-design refine results sect for a cleaner layout

--- jobseek/forms.py
+++ jobseek/forms.py
@@ -48,4 +48,4 @@ class refineForm(FlaskForm):
     sector = QuerySelectField(label='Job Type', blank_text='Sector', allow_blank=True, query_factory=choice, get_label=lambda a: a.sector)
     salary = QuerySelectField(label='Job Type', blank_text='Salary', allow_blank=True, query_factory=choice, get_label=lambda a: a.salary)
     location = QuerySelectField(label='Job Type', blank_text='Location', allow_blank=True, query_factory=choice, get_label=lambda a: a.location)
-    submit = SubmitField('Update')
+    submit = SubmitField('Refine')
