101533b998838014246782eef2dc284619397a2d
Matt Bush <bush.matthewjames@gmail.com>
2019-05-07 15:40:03 +0100

    Add location db table for location choices
    User will now choose from a pre-defined list of locations
    rather than manual input. For easier grouping of data

--- jobseek/models.py
+++ jobseek/models.py
@@ -7,7 +7,7 @@ from flask_login import UserMixin
 def user_loader(user_id):
     return employer.query.get(user_id)
 
-# create employer user db model 
+# employer user db model 
 class employer(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     companyName = db.Column(db.String(80), unique=True, nullable=False)
@@ -18,26 +18,34 @@ class employer(db.Model, UserMixin):
     def __repr__(self):
         return f"User: {self.companyName}"
 
-# create job_post db model
+# job_post db model
 class job_post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     title = db.Column(db.String(60), nullable=False)
     jobType = db.Column(db.String(40), nullable=False)
-    location = db.Column(db.String(60), nullable=False)
     role_sum = db.Column(db.Text, nullable=False)
     resp = db.Column(db.Text, nullable=False)
     requirements = db.Column(db.Text, nullable=False)
     how_to_apply = db.Column(db.Text, nullable=False)
     emp_id = db.Column(db.Integer, db.ForeignKey('employer.id'), nullable=False )
     sector_id = db.Column(db.Integer, db.ForeignKey('sector.id'), nullable=False )
+    location_id = db.Column(db.Integer, db.ForeignKey('location.id'), nullable=False )
     # string to return in shell when job_post model called
     def __repr__(self):
         return f"Job Post: {self.date_posted}, {self.title}"
 
+# sector db model to store sectors choice
 class sector(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     sector = db.Column(db.String(60), nullable=False)
     def __repr__(self):
         return f"Sector: {self.id}, {self.sector}"
 
+# location db model to store location choice
+class location(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    city = db.Column(db.String(30), nullable=False)
+    country = db.Column(db.String(10), nullable=False)
+    def __repr__(self):
+        return f"Sector: {self.city}, {self.country}"
