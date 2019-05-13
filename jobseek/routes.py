3144040d494e4101ee81a18cd468c0d836050716
Matt Bush <bush.matthewjames@gmail.com>
2019-05-13 16:27:39 +0100

    Update routes.py comments
    Code readability

--- jobseek/routes.py
+++ jobseek/routes.py
@@ -24,13 +24,18 @@ def choices():
             location_choices.append((g.location_ref.id, g.location_ref.city))
     return salary_choices, sector_choices, jobType_choices, location_choices
 
-
+# index route for home page
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    page = request.args.get('page', 1, type=int)
+    page = request.args.get('page', 1, type=int) # find page for pagination 
     job_posts = job_post.query.order_by(job_post.date_posted.desc()).paginate(page=page, per_page=2)
-    job_posts_total = job_posts.total
+    job_posts_total = job_posts.total # total posts for refine results 
     form = refineForm()
+    # refine form select choices from choices helper function
+    form.salary.choices = choices()[0]
+    form.sector.choices = choices()[1]
+    form.jobType.choices = choices()[2]
+    form.location.choices = choices()[3]
 
     # current filters placeholders
     jobTypeField = form.jobType.data
@@ -38,14 +43,9 @@ def index():
     sectorField = form.sector.data
     locationField = form.location.data
 
-    # refine form select choices from choices helper function
-    form.salary.choices = choices()[0]
-    form.sector.choices = choices()[1]
-    form.jobType.choices = choices()[2]
-    form.location.choices = choices()[3]
-
+    # refine results form handler
+    # checking if field selected. If not return all posts
     if form.validate_on_submit():
-        # form handling of none selected 
         if form.jobType.data == 'jobType':
             jobType = job_post.jobType
         else:
@@ -65,6 +65,7 @@ def index():
             location_id = job_post.location_id
         else:
             location_id = form.location.data
+
         # new job_posts with filters
         job_posts = job_post.query.filter(and_(job_post.jobType == jobType,
                                                 job_post.sector_id == sector_id,
@@ -79,8 +80,12 @@ def index():
     return render_template('home.html', job_posts=job_posts,  job_posts_total= job_posts_total, form=form, jobTypeField=jobTypeField, 
                             salaryField=salaryField, sectorField=sectorField, locationField=locationField)
 
+# register page 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    ''' If current logged in user tries to access register page via URL then redirect to home page.
+        If unregistered and register form validates, create and commit new employer to db
+    '''
     if current_user.is_authenticated:
         return redirect('home')
     form = registerForm()
@@ -97,9 +102,13 @@ def register():
     
     return render_template('register.html', form=form)
     
-
+# login page
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    ''' If current logged in user tries to access login page via URL then redirect to home page.
+        If user is unregistered then send to register page and flash message.
+        If user is registered then login user and redirect to home page
+    '''
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = loginForm()
@@ -112,15 +121,19 @@ def login():
             flash(f'Login unsuccessful. Please check company name and email', 'danger')
     return render_template('login.html', form=form)
 
+# logout user
 @app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for('index'))
 
-
+# create job page
 @app.route("/create_job/new", methods=['GET', 'POST'])
 @login_required
 def create_job():
+    ''' If all form fields are filled in and validates then create and commit a new job post to db.
+        Redirecet to home page with flash message success
+    '''
     form = jobForm()
     if form.validate_on_submit():
         new_job = job_post(title=form.title.data, sector_id=form.sector.data.id, jobType=form.jobType.data, location_id=form.location.data.id ,
@@ -132,7 +145,7 @@ def create_job():
         return redirect(url_for('index'))
     return render_template(('create_job.html'), form=form, title='Create Job Post')
 
-# # view a full job post
+# view a full job post
 @app.route("/job/<int:id>")
 def job(id):
     job = job_post.query.get(id)
