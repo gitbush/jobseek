73fdd339dac28468e00e388156ab8b38c532277d
Matt Bush <bush.matthewjames@gmail.com>
2019-05-14 21:03:16 +0100

    Updatte app.py port and host for deployment
    Port and host issues with heroku

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ import os
 from jobseek import app, routes
 
 if __name__ == "__main__":
-    app.run(host=os.getenv("IP"),
-            port=os.getenv("PORT"),
-            debug=True)
+     # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
