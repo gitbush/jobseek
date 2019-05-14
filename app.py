22d2162b5852285d973d1d74549008f2e59b2190
Matt Bush <bush.matthewjames@gmail.com>
2019-05-14 09:02:50 +0100

    Change port variable for heroku

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ import os
 from jobseek import app, routes
 
 if __name__ == "__main__":
-    app.run(host=os.getenv("IP"),
+    app.run(host=("0.0.0.0"),
             port=os.getenv("PORT"),
             debug=True)
