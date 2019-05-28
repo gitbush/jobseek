cc30bf407f21a2365e0e5b0b74afa82e379dc9ab
Matt Bush <bush.matthewjames@gmail.com>
2019-05-28 16:19:08 +0100

    Update config.py
    Change to JAWSDB_URL for ease of deployment with heroku

--- jobseek/config.py
+++ jobseek/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.urandom(32)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('JAWSDB_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
