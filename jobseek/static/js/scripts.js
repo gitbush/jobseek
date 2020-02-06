bee7e9d6a83792de5322f809088d5ceda217fd8e
Matt Bush <bush.matthewjames@gmail.com>
2020-02-06 16:58:45 +0000

    Remove buggy refine btn script
    Disable button on change of form was not working properly

--- jobseek/static/js/scripts.js
+++ jobseek/static/js/scripts.js
@@ -1,16 +1,4 @@
 $(document).ready(function(){
-    
-    // disable refine button if none selected
-    $('.btn-refine').attr('disabled', true);
-    $('#sector, #salary, #location, #jobType').change(function(){
-        if(this.value != '0'){
-        $('.btn-refine').attr('disabled', false);
-        }
-        else {
-            $('.btn-refine').attr('disabled', true);
-            
-        }
-    }) 
 
     // replace all text area fields with CKeditor
     CKEDITOR.replaceAll();
