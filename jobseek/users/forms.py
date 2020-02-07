bf4c987d3b4b2bdc5d0bef5d00c089497bee0b00
Matt Bush <bush.matthewjames@gmail.com>
2020-02-07 12:08:04 +0000

    Fix register form val not working
    Custom validators not working

--- jobseek/users/forms.py
+++ jobseek/users/forms.py
@@ -3,29 +3,34 @@ from wtforms import StringField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, ValidationError 
 from jobseek.models import employer
 
-# wtforms registration class
 class RegisterForm(FlaskForm):
+    """
+    User registration form
+    """
     companyName = StringField('Company Name', validators=[DataRequired(), Length(min=3, max=30)])
     companyEmail = StringField('Company Email', validators=[DataRequired(), Email()])
     logo = StringField('Profile Logo URL')
     submit = SubmitField('Register')
 
     # custom validation checking if a user has already signed up with company name and email
-    def validate_company_name(self, companyName):
+    def validate_companyName(self, companyName):
         ''' If details already registered, show error msg asking to login '''
+
         emp = employer.query.filter_by(companyName=companyName.data).first()
         if emp:
             raise ValidationError('Company already exists on Jobseek. Please login')
 
-    def validate_email(self, email):
+    def validate_companyEmail(self, companyEmail):
         ''' If details already registered, show error msg asking to use alternate email '''
+
         emp = employer.query.filter_by(email=companyEmail.data).first()
         if emp:
             raise ValidationError('Email already exists on Jobseek. Please use an alternate email')
 
-
-# wtforms login class
 class LoginForm(FlaskForm):
+    """
+    User login form
+    """
     companyName = StringField('Company Name', validators=[DataRequired(), Length(min=3, max=30)])
     companyEmail = StringField('Company Email', validators=[DataRequired(), Email()])
     remember = BooleanField('Remember Me')
