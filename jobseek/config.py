c1c9ad83387a52f4d154fcf3715746631999521e
Matt Bush <bush.matthewjames@gmail.com>
2020-02-08 20:28:27 +0000

    Change database uri info
    More comments and relevant naming of env variable

--- jobseek/config.py
+++ jobseek/config.py
@@ -2,5 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.urandom(32)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('JAWSDB_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+
+    # untrack any changes to objects. Stop long message in terminal when running app
     SQLALCHEMY_TRACK_MODIFICATIONS = False
